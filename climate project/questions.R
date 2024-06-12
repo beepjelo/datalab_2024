48a3844e5e933306e3ec1f91b6afc689eb323aec
nicole <mac@nicole-2.local>
2024-06-12 14:17:36 -0500

    Climate

---
+++ climate project/questions.R
@@ -0,0 +1,15 @@
+#I am interested in climate at Sewanee. Climate change is interesting. Rainfall is cool. I don't know I just like the environment. Can you tell me some stuff about it?
+
+# 1. How does Sewanee's average temperature change over time?
+# 2. How does the range (max. and min.) of temperature change over time?
+# 3. How much of Sewanee's precipitation changes on average over time?
+# 4. How much does it snow on average in Sewanee over time?
+# 5. What is the average depth of snow at Sewanee?
+
+#----
+#1. Average precipitation fro each moth in five year periods
+#2. Effect of temperature on precipitation frm 2010 to 2020
+#3. Minimum and maximum temperature over time for the entire period
+#4. How was snowfall chanegd over time for the entire period?
+#5. Average temperature over time
+#6. What month has the most snowfall on average
